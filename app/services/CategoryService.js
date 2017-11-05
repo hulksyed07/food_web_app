6d09874f3c190baf368cd0fd49c3d76f51af08b8
hulksyed07 <hulk.syed07@gmail.com>
2017-11-05 12:42:50 +0530

    Reading token from sessionStorage

--- app/services/CategoryService.js
+++ app/services/CategoryService.js
@@ -3,9 +3,10 @@ angular.module('IndiaEats').service('CategoryService', function($http, UtilsServ
 
 	service.getAllCategories = function() {
 		var headers = new Headers;
-		headers['Access-Token'] = localStorage.getItem('access_token');
+		headers['Access-Token'] = sessionStorage.getItem('access_token');
 		return $http.get('http://localhost:3000/v1/categories', {headers: headers})
 				.then(function(result){
+					console.log(result);
 					return UtilsService.objectToArray(result.data);
 				})
 	};
