d6ef2fc84f0c51c6c6b5606f7ba28e1003fea94a
hulksyed07 <hulk.syed07@gmail.com>
2017-11-23 20:19:17 +0530

    Configuring Karma

---
+++ spec/controllers/AuthenticationController.js
@@ -0,0 +1,9 @@
+describe('AuthenticationController', function(){
+  // var  AuthenticationService = require('../../app/services/AuthenticationService');
+  var  AuthenticationController = require('../../app/controllers/AuthenticationController');
+
+  it('should compare 1 with 1', function(){
+    expect(1).toBe(1);
+  })
+
+})
